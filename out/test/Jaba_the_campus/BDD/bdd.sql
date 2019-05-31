8a612bdb8206cfb26063d18a4d7844a45119f93b
Victor <victor.aymard@edu.ece.fr>
2019-05-31 17:11:31 +0200

    JavaDoc reporting
    écriture de la javaDoc pour le fichier reporting + génération de la JavaDoc

--- out/test/Jaba_the_campus/BDD/bdd.sql
+++ out/test/Jaba_the_campus/BDD/bdd.sql
@@ -4,22 +4,6 @@ CREATE TABLE Ecole(
 	Annee_Fondation int(11) NOT NULL
 );
 
-<<<<<<< HEAD
-CREATE TABLE Classe(
-    ID_Classe int(11) PRIMARY KEY AUTO_INCREMENT,
-    Niveau varchar(255) NOT NULL,
-    ID_Ecole int(11),
-    ID_AnneeScolaire int(11),
-    FOREIGN KEY (ID_Ecole) REFERENCES Ecole(ID_Ecole),
-    FOREIGN KEY (ID_AnneeScolaire) REFERENCES Ecole(ID_AnneeScolaire)
-);
-
-CREATE TABLE AnneeScolaire(
-    ID_AnneeScolaire int(11) PRIMARY KEY AUTO_INCREMENT,
-    Debut_AnneeScolaire int(11) NOT NULL,
-    Fin_AnneeScolaire int(11) NOT NULL
-);
-=======
 CREATE TABLE AnneeScolaire(
     ID_AnneeScolaire int(11) PRIMARY KEY AUTO_INCREMENT,
     Debut_AnneeScolaire int(11) NOT NULL,
@@ -36,82 +20,33 @@ CREATE TABLE Classe(
 );
 
 
->>>>>>> master
 
 CREATE TABLE Trimestre(
     ID_Trimestre int(11) PRIMARY KEY AUTO_INCREMENT,
     Numero int(11) NOT NULL,
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-    Debut_Trimestre int(11) NOT NULL,
-    Debut_Trimestre int(11) NOT NULL,
-=======
     Debut_Trimestre date NOT NULL,
     Fin_Trimestre date NOT NULL,
->>>>>>> master
-=======
-    Debut_Trimestre date NOT NULL,
-    Fin_Trimestre date NOT NULL,
->>>>>>> Stashed changes
     ID_AnneeScolaire int(11),
     FOREIGN KEY (ID_AnneeScolaire) REFERENCES AnneeScolaire(ID_AnneeScolaire)
 );
 
 CREATE TABLE Bulletin(
     ID_Bulletin int(11) PRIMARY KEY AUTO_INCREMENT,
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
-    Moyenne_Trimestre float NOT NULL,
->>>>>>> Stashed changes
-    ID_Trimestre int(11),
-    ID_Eleve int(11),
-    Appreciation_Bulletin varchar(255) NOT NULL,
-    FOREIGN KEY (ID_Trimestre) REFERENCES Trimestre(ID_Trimestre),
-<<<<<<< Updated upstream
-    FOREIGN KEY (ID_Effectif) REFERENCES Effectif(ID_Effectif)
-=======
     Moyenne_Trimestre float NOT NULL,
     ID_Trimestre int(11),
     ID_Eleve int(11),
     Appreciation_Bulletin varchar(255) NOT NULL,
     FOREIGN KEY (ID_Trimestre) REFERENCES Trimestre(ID_Trimestre),
     FOREIGN KEY (ID_Eleve) REFERENCES Eleve(ID_Eleve)
->>>>>>> master
-=======
-    FOREIGN KEY (ID_Eleve) REFERENCES Eleve(ID_Eleve)
->>>>>>> Stashed changes
 );
 
 CREATE TABLE DetailBulletin(
     ID_DetailBulletin int(11) PRIMARY KEY AUTO_INCREMENT,
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
     Moyenne_Enseignement float NOT NULL,
->>>>>>> master
-=======
-    Moyenne_Enseignement float NOT NULL,
->>>>>>> Stashed changes
     ID_Bulletin int(11),
     ID_Enseignement int(11),
     Appreciation_DetailBulletin varchar(255) NOT NULL,
     FOREIGN KEY (ID_Bulletin) REFERENCES Bulletin (ID_Bulletin),
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-    FOREIGN KEY (ID_Cours) REFERENCES Enseignement(ID_Enseignement)
-=======
-    FOREIGN KEY (ID_Enseignement) REFERENCES Enseignement(ID_Enseignement)
->>>>>>> Stashed changes
-);
-
-CREATE TABLE Devoir(
-    ID_Devoir int(11) PRIMARY KEY AUTO_INCREMENT,
-    ID_DetailBulletin int(11),
-    Note int(11) NOT NULL,
-<<<<<<< Updated upstream
-    Appreciation_Evaluation varchar(255) NOT NULL,
-=======
     FOREIGN KEY (ID_Enseignement) REFERENCES Enseignement(ID_Enseignement)
 );
 
@@ -120,10 +55,6 @@ CREATE TABLE Devoir(
     ID_DetailBulletin int(11),
     Note int(11) NOT NULL,
     Appreciation_Devoir varchar(255) NOT NULL,
->>>>>>> master
-=======
-    Appreciation_Devoir varchar(255) NOT NULL,
->>>>>>> Stashed changes
     FOREIGN KEY (ID_DetailBulletin) REFERENCES DetailBulletin(ID_DetailBulletin)
 );
 
@@ -136,22 +67,6 @@ CREATE TABLE Enseignement(
     ID_Enseignement int(11) PRIMARY KEY AUTO_INCREMENT,
     ID_Classe int(11),
     ID_Discipline int(11),
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-    ID_Personne int(11),
-=======
-    ID_Enseignant int(11),
->>>>>>> Stashed changes
-    FOREIGN KEY (ID_Classe) REFERENCES Classe(ID_Classe),
-    FOREIGN KEY (ID_Discipline) REFERENCES Discipline(ID_Discipline),
-    FOREIGN KEY (ID_Enseignant) REFERENCES Enseignant(ID_Enseignant)
-);
-
-<<<<<<< Updated upstream
-CREATE TABLE Personne(
-    ID_Personne int(11) PRIMARY KEY AUTO_INCREMENT,
-    Type varchar(255) NOT NULL,
-=======
     ID_Enseignant int(11),
     FOREIGN KEY (ID_Classe) REFERENCES Classe(ID_Classe),
     FOREIGN KEY (ID_Discipline) REFERENCES Discipline(ID_Discipline),
@@ -160,35 +75,12 @@ CREATE TABLE Personne(
 
 CREATE TABLE Enseignant(
     ID_Enseignant int(11) PRIMARY KEY AUTO_INCREMENT,
->>>>>>> master
-=======
-CREATE TABLE Enseignant(
-    ID_Enseignant int(11) PRIMARY KEY AUTO_INCREMENT,
->>>>>>> Stashed changes
     Nom varchar(255) NOT NULL,
     Prenom varchar(255) NOT NULL,
     Login varchar(255) NOT NULL,
     Mdp varchar(255) NOT NULL
 );
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-CREATE TABLE Inscription(
-    ID_Inscription int(11) PRIMARY KEY AUTO_INCREMENT,
-=======
-CREATE TABLE Eleve(
-    ID_Eleve int(11) PRIMARY KEY AUTO_INCREMENT,
-    Nom varchar(255) NOT NULL,
-    Prenom varchar(255) NOT NULL,
-    Login varchar(255) NOT NULL,
-    Mdp varchar(255) NOT NULL,
->>>>>>> Stashed changes
-    ID_Classe int(11),
-    FOREIGN KEY (ID_Classe) REFERENCES Classe(ID_Classe)
-);
-
-<<<<<<< Updated upstream
-=======
 CREATE TABLE Eleve(
     ID_Eleve int(11) PRIMARY KEY AUTO_INCREMENT,
     Nom varchar(255) NOT NULL,
@@ -199,8 +91,6 @@ CREATE TABLE Eleve(
     FOREIGN KEY (ID_Classe) REFERENCES Classe(ID_Classe)
 );
 
-=======
->>>>>>> Stashed changes
 INSERT INTO Ecole VALUES (1, 'Ece Paris', 1919);
 
 INSERT INTO Enseignant VALUES (1, 'Rossard', 'Jacques', 'JacquesRossard', 'oklm');
@@ -258,7 +148,3 @@ INSERT INTO Devoir VALUES (9, 9, 12, 'Assez Bien');
 
 
 
-<<<<<<< Updated upstream
->>>>>>> master
-=======
->>>>>>> Stashed changes
--- src/View/Reporting.java
+++ src/View/Reporting.java
@@ -6,6 +6,8 @@
  * Jaba_the_utt\lib\JFreeChart--> clique sur apply, puis sur ok
  */
 
+
+
 package View;
 
 import org.jfree.chart.ChartFactory;
@@ -15,14 +17,32 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
+
+/**
+ * <b>Reporting est la classe dedie a l'affichage de statistiques</b>
+ *  <br>
+ *
+ *     Les statistiques vont etre affichees de plusieurs manieres :
+ *      <ul>
+ *          <li>sous forme de pie chart</li>
+ *          <li>sous forme de diagramme en barre</li>
+ *          <li>sous forme de diagramme batton</li>
+ *      </ul>
+ *
+ * @author victor
+ * @version 1.0
+ */
 //TODO des charts dynamiques qui ont un nombre de pics variables
 public class Reporting  {
 
 
-
-
-
-
+    /**
+     * <b>Fonction test des graphiques</b>
+     * <br>
+     *     <p>Cette fonction permet de tester les differents graphiques qu il est possible de realiser</p>
+     * @param args
+     *              parametre de base de la fonction main
+     */
     public static void main(String[] args) {
 
 
@@ -65,6 +85,31 @@ public class Reporting  {
 
 
     //méthodes
+
+    /**
+     * <b>Creation d'un diagramme en barre double avec 3 instances (en x)</b>
+     * <br>
+     * <p>Ce diagramme compare deux entitees sur 3 instances differentes
+     * <br>Par exemple, professeur et eleves sur 3 années différentes</p>
+     * @param categorie1 premiere categoreie d'entite
+     * @param categorie2 seconde categorie d'entite
+     * @param column1 premiere instance
+     * @param column2 seconde instance
+     * @param column3 troisieme instance
+     * @param valueCat1Col1 valeur de la categorie 1 colonne 1
+     * @param valueCat1Col2 valeur de la categorie 1 colonne 2
+     * @param valueCat1Col3 valeur de la categorie 1 colonne 3
+     * @param valueCat2Col1 valeur de la categorie 2 colonne 1
+     * @param valueCat2Col2 valeur de la categorie 2 colonne 2
+     * @param valueCat2Col3 valeur de la categorie 2 colonne 3
+     * @param titrefenetre titre de la fenetre ou est represente le graphique
+     * @param titreChart titre du graphique
+     * @param xLabel legende de l'axe de abscisses
+     * @param ylLabel legende de l'axe de ordonnees
+     * @param legend booleen d'affiche des legendes
+     * @param tooltips booleen d'affiche des outils
+     * @param urls booleen d'affiche de urls
+     */
     public static void barChartDouble3parts(String categorie1, String categorie2,
                                             String column1, String column2, String column3,
                                             double valueCat1Col1, double valueCat1Col2, double valueCat1Col3,
@@ -95,11 +140,31 @@ public class Reporting  {
         frame.setVisible(true);
     }
 
-
+    /**
+     * <b>Creation d'un diagramme en barre simple avec 3 instances (en x)</b>
+     *      * <br>
+     *      * <p>Ce diagramme indique une entite sur 3 instances differentes
+     *      * <br>Par exemple, les eleves sur 3 années différentes</p>
+     *
+     * @param categorie1 premiere categoreie d'entite
+     * @param categorie2 seconde categorie d'entite
+     * @param column1 premiere instance
+     * @param column2 seconde instance
+     * @param column3 troisieme instance
+     * @param valueCat1Col1 valeur de la categorie 1 colonne 1
+     * @param valueCat1Col2 valeur de la categorie 1 colonne 2
+     * @param valueCat1Col3 valeur de la categorie 1 colonne 3
+     * @param titrefenetre titre de la fenetre ou est represente le graphique
+     * @param titreChart titre du graphique
+     * @param xLabel legende de l'axe de abscisses
+     * @param ylLabel legende de l'axe de ordonnees
+     * @param legend booleen d'affiche des legendes
+     * @param tooltips booleen d'affiche des outils
+     * @param urls booleen d'affiche de urls
+     */
     public static void barChartSimple3parts(String categorie1, String categorie2,
                                             String column1, String column2, String column3,
                                             double valueCat1Col1, double valueCat1Col2, double valueCat1Col3,
-                                            double valueCat2Col1, double valueCat2Col2, double valueCat2Col3,
                                             String titrefenetre, String titreChart, String xLabel, String ylLabel,
                                             boolean legend, boolean tooltips, boolean urls){
 
@@ -125,10 +190,19 @@ public class Reporting  {
     }
 
 
-
-
-
-
+    /**
+     * <b>Creation d'un diagramme circulaire (camembert) avec 2 instances</b>
+     * <br>
+     * <p>Ce diagramme compare deux entitees sur 2 instances differentes</p>
+     * @param categorie1 premiere categoreie d'entite
+     * @param value1 valeur de liee a la premiere categorie d'entite
+     * @param categorie2 seconde categoreie d'entite
+     * @param value2 valeur de liee a la seconde categorie d'entite
+     * @param titreChart titre du graphique
+     * @param legend booleen d'affiche des legendes
+     * @param tooltips booleen d'affiche des outils
+     * @param urls booleen d'affiche de urls
+     */
     public static void pieChart2parts(String categorie1, double value1,
                                       String categorie2, double value2,
                                       String titreFenetre,
@@ -151,6 +225,22 @@ public class Reporting  {
         frame.setVisible(true);
     }
 
+    /**
+     * <b>Creation d'un diagramme circulaire (camembert) avec 3 instances</b>
+     * <br>
+     * <p>Ce diagramme compare deux entitees sur 3 instances differentes</p>
+     *
+     * @param categorie1 premiere categoreie d'entite
+     * @param value1 valeur de liee a la premiere categorie d'entite
+     * @param categorie2 seconde categoreie d'entite
+     * @param value2 valeur de liee a la seconde categorie d'entite
+     * @param categorie3 troisieme categoreie d'entite
+     * @param value3  valeur de liee a la seconde categorie d'entite
+     * @param titreChart titre du graphique
+     * @param legend booleen d'affiche des legendes
+     * @param tooltips booleen d'affiche des outils
+     * @param urls booleen d'affiche de urls
+     */
     public static void pieChart3parts(String categorie1, double value1,
                                       String categorie2, double value2,
                                       String categorie3, double value3,
