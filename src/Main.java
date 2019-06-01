59274f4db50d289e379ea30e46d6a4e54d970716
Thomas <thomas.lemercier@edu.ece.fr>
2019-06-01 17:53:58 +0200

    remise du main

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ import View.Acceuil.AccueilFrame;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         String nombdd = "projetjava";
         String user = "root";
         String passwd = "";
         View.Acceuil.AccueilFrame acceuil = new AccueilFrame();
+
+        Connexion test = new Connexion(nombdd, user, passwd);
 
     }
 }
