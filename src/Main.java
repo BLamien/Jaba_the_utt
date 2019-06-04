ea04414e782e3dac7fe07e56e3d30760bb195f59
Thomas <thomas.lemercier@edu.ece.fr>
2019-06-04 12:44:05 +0200

    javadoc + package DAO début + test récup dataBDD

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import View.Acceuil.AcceuilCenter;
 import View.Acceuil.AccueilFrame;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
@@ -13,6 +14,15 @@ public class Main {
         View.Acceuil.AccueilFrame acceuil = new AccueilFrame();
 
         Connexion test = new Connexion(nombdd, user, passwd);
+
+        ArrayList<String> Liste;
+        String table = "eleve";
+        Liste = test.remplirChampsTable(table);
+
+        for(int i=0; i<Liste.size(); i++)
+        {
+            System.out.print(Liste.get(i)+ " ");
+        }
 
     }
 }
